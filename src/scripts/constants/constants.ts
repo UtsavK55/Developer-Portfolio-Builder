ea7aa6a23c2d7e7afa4f480965a01035afd8fb5e
UtsavK55 <utsav@gkmit.co>
2024-09-27 18:11:52 +0530

    fix:resolve comments

--- src/scripts/constants/constants.ts
+++ src/scripts/constants/constants.ts
@@ -1,1 +1,2 @@
 export const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const boolValuesforSorting = { true: 1, false: 2 };
--- src/scripts/project.ts
+++ src/scripts/project.ts
@@ -2,7 +2,6 @@ const projectName = document.getElementById("projectName") as HTMLInputElement;
 const techStack = document.getElementById("techStack") as HTMLInputElement;
 const isCompleted = document.getElementById("isCompleted") as HTMLSelectElement;
 
-// Add Project
 export const addProject = (
   event: Event,
   developer: Developer,
@@ -13,21 +12,22 @@ export const addProject = (
   project.projectName = projectName.value;
   techStack.value.split(",").map((tech) => project.techStack.push(tech));
   project.isCompleted = isCompleted.value === "yes";
+
   developer?.projects.push({ ...project });
+
   resetProjectForm(project);
 };
 
-// Reset form
 export const resetProjectForm = (project: Project) => {
   projectName.value = "";
   techStack.value = "";
   isCompleted.value = "yes";
+
   project.projectName = "";
   project.techStack = [];
   project.isCompleted = true;
 };
 
-// Validate Project
 export const validateProject = () => {
   if (techStack.value.split(",").length < 2) {
     alert(
@@ -38,25 +38,26 @@ export const validateProject = () => {
   return true;
 };
 
-// list Projects
 export const listProjects = (developer: Developer) => {
   const { projects } = developer;
-  if (projects.length === 0) {
+  if (!projects.length) {
     alert("No projects exist");
     return;
   }
   return projects;
 };
 
-// Count Completed Projects
 export const countCompletedProjects = (developer: Developer) => {
   const { projects } = developer;
-  if (projects.length === 0) {
+  if (!projects.length) {
     alert("No project exist");
     return;
   }
-  let counter: number = 0;
-  projects.forEach((project) => project.isCompleted && counter++);
+
+  const counter = projects.reduce((acc, project) => {
+    return project.isCompleted ? acc + 1 : acc;
+  }, 0);
+
   if (counter === 0) {
     alert("No completed project exists");
   } else {
